8c5982f18b77e326fe623543f1bd880c147c5a00
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2023-09-26 17:42:44 +0600

    if click button (Link) of afterDonationCard component, it takes me to the CardDetails component (this whole task is completed)

--- src/components/afterDonation/AfterDonationCard.jsx
+++ src/components/afterDonation/AfterDonationCard.jsx
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+
 const AfterDonationCard = ({data}) => {
     const {picture, title, category, category_bg, card_bg, text_button_bg, price} = data
-    // console.log(id)
 
   return (
-    <div className=' flex cursor-pointer active:drop-shadow-2xl lg rounded-lg' style={{ backgroundColor: card_bg }}>
+    <div className=' flex active:drop-shadow-2xl lg rounded-lg' style={{ backgroundColor: card_bg }}>
         <div>
             <img src={picture} alt="" className='h-full w-40 rounded-s-lg' />
         </div>
@@ -19,7 +19,7 @@ const AfterDonationCard = ({data}) => {
                     <p className='mb-5 font-semibold' style={{ color: text_button_bg}}>{price}</p>
                 </div>
                 <div>
-                    <Link to="#" className=' text-white px-4 py-2 rounded' style={{ backgroundColor: text_button_bg}}>View Details</Link>
+                    <Link to={`/card-details/${title}`} className=' text-white px-4 py-2 rounded' style={{ backgroundColor: text_button_bg}}>View Details</Link>
                 </div>
             </div>
     </div>
